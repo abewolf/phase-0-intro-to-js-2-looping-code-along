4fdaa9aff181f33dca07ec62730bd94593c2d32d
Abraham B Martinez <abeywolfpack@gmail.com>
2023-02-15 00:01:22 -0800

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let messagesArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+        messagesArray.push(message)
+    }
+    return messagesArray;
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--){
+        console.log(i)
+    }
+}
+    
+
+
+    
